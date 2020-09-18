b1c45fee2c2949bb15ef72c6cbfc4fe399d0f492
Thomas Christiansen <thomaschristiansen@Thomas-MacBook-Pro.local>
2020-09-19 00:03:13 +0200

    Formatted shops.config.js

--- shops.config.js
+++ shops.config.js
@@ -1,46 +1,50 @@
 const shops = [
-    {
-        name: 'Power',
-        url: 'https://www.power.dk/gaming-og-underholdning/konsol/konsol/playstation-5/p-1077687',
-        outOfStockMessage: 'Ikke på lager online',
-        addToCartSelector: '.buy-area__webshop .btn-mega'
-    },
-    {
-        name: 'Expert',
-        url: 'https://www.expert.dk/spil-og-underholdning/konsoller/konsoller/playstation-5/p-1077687',
-        outOfStockMessage: 'Ikke på lager online',
-        addToCartSelector: '.buy-area__webshop .btn-mega'
-    },
-    {
-        name: 'Elgiganten',
-        url: 'https://www.elgiganten.dk/product/gaming/konsoller/playstation-4-ps4-konsol/220276/playstation-5',
-        outOfStockMessage: 'Ikke tilgængelig',
-        addToCartSelector: '.button.add-to-basket'
-    },
-    {
-        name: 'Komplett',
-        url: 'https://www.komplett.dk/product/1111557/gaming/playstation/playstation-5',
-        outOfStockMessage: 'Modtag varsel',
-        addToCartSelector: '.product-main-info-buy .btn-large'
-    },
-    {
-        name: 'Bilka',
-        url: 'https://www.bilka.dk/produkter/sony-playstation-5/100532624',
-        outOfStockMessage: 'Udsolgt',
-        addToCartSelector: '.purchase-button'
-    },
-    {
-        name: 'Føtex',
-        url: 'https://www.foetex.dk/produkter/sony-playstation-5/100532624',
-        outOfStockMessage: 'Udsolgt',
-        addToCartSelector: '.purchase-button'
-    },
-    {
-        name: 'BR',
-        url: 'https://www.br.dk/produkter/sony-playstation-5/100532624',
-        outOfStockMessage: 'Udsolgt',
-        addToCartSelector: '.purchase-button'
-    }
-]
+  {
+    name: "Power",
+    url:
+      "https://www.power.dk/gaming-og-underholdning/konsol/konsol/playstation-5/p-1077687",
+    outOfStockMessage: "Ikke på lager online",
+    addToCartSelector: ".buy-area__webshop .btn-mega",
+  },
+  {
+    name: "Expert",
+    url:
+      "https://www.expert.dk/spil-og-underholdning/konsoller/konsoller/playstation-5/p-1077687",
+    outOfStockMessage: "Ikke på lager online",
+    addToCartSelector: ".buy-area__webshop .btn-mega",
+  },
+  {
+    name: "Elgiganten",
+    url:
+      "https://www.elgiganten.dk/product/gaming/konsoller/playstation-4-ps4-konsol/220276/playstation-5",
+    outOfStockMessage: "Ikke tilgængelig",
+    addToCartSelector: ".button.add-to-basket",
+  },
+  {
+    name: "Komplett",
+    url:
+      "https://www.komplett.dk/product/1111557/gaming/playstation/playstation-5",
+    outOfStockMessage: "Modtag varsel",
+    addToCartSelector: ".product-main-info-buy .btn-large",
+  },
+  {
+    name: "Bilka",
+    url: "https://www.bilka.dk/produkter/sony-playstation-5/100532624",
+    outOfStockMessage: "Udsolgt",
+    addToCartSelector: ".purchase-button",
+  },
+  {
+    name: "Føtex",
+    url: "https://www.foetex.dk/produkter/sony-playstation-5/100532624",
+    outOfStockMessage: "Udsolgt",
+    addToCartSelector: ".purchase-button",
+  },
+  {
+    name: "BR",
+    url: "https://www.br.dk/produkter/sony-playstation-5/100532624",
+    outOfStockMessage: "Udsolgt",
+    addToCartSelector: ".purchase-button",
+  },
+];
 
 module.exports = shops;
